1b365ed8e02ce3c0fea96bcfcd5bc70c9d7d120d
millerj-tc <j.sam.miller@gmail.com>
2022-12-29 08:41:23 -0600

    card search website

---
+++ buildCardObjects/buildCardObjectsFlow.js
@@ -0,0 +1,4 @@
+export function buildCardObjectsFlow()
+{
+ //make sure to dynamically create labels from first line of csv so that change in label order doesn't break everything   
+}
---
+++ cardData.js
@@ -0,0 +1,6 @@
+	`Doran, the Necrodude	Lv.1 Character	1rsrc&		1 wp&		grand&: +2 wp& this turn if you have at least 5 cards in your discard.	tcgpics\prototype pixel art\necromancer.png	C:\nandeck(1)\tcgpics\prototype pixel art\necromancer.png			Character - Doran	1	Joseph Miller	%%%%												
+	Swift Travel	Event	3rsrc&				"Draw two cards and put 1wp& on a quest Lv.1 or lower.
+
+If you control two or fewer characters, draw an additional card."	tcgpics\cc0 icons\S_Wind06.png	C:\nandeck(1)\tcgpics\cc0 icons\S_Wind06.png			Event		Joseph Miller	%%%%												
+	Enchanted Bident	Support	3rsrc&				"rsrc&, exhaust: Draw a card or +2wp& to any character. 
+(Supports can be exhausted for 1 rsrc&)"	tcgpics\prototype pixel art\bident.png	C:\nandeck(1)\tcgpics\prototype pixel art\bident.png			Support		Joseph Miller	%%%%`											
