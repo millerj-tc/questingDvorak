5496d3c10ec8571553ecbbda2f9b47625ed3e917
millerj-tc <j.sam.miller@gmail.com>
2022-12-31 17:12:38 -0600

    live bug

--- onLoad.js
+++ onLoad.js
@@ -1,4 +1,4 @@
-import {BuildCardObjectsFlow} from './buildCardObjects/BuildCardObjectsFlow.js';
+import {BuildCardObjectsFlow} from "./buildCardObjects/BuildCardObjectsFlow.js";
 import {CardSearchFlow} from "./cardSearch/cardSearchFlow.js";
 
 export function OnLoad(){
